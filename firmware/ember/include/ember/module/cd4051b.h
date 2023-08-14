50ccb90dfe3fea21d39aff8cdbacad9352acd969
Ken Yoshizoe <kenyoshizoe@gmail.com>
2023-08-14 16:20:50 +0900

    :sparkles: add class for cb4051b

---
+++ firmware/ember/include/ember/module/cd4051b.h
@@ -0,0 +1,27 @@
+#ifndef EMBER_MODULE_CD4051B_H
+#define EMBER_MODULE_CD4051B_H
+
+#include "main.h"
+
+namespace ember {
+/**
+ * @brief Class for controlling a 8 channel analog multiplexer CD4051B.
+ */
+class CD4051B {
+ public:
+  CD4051B(GPIO_TypeDef* gpio_a_port, uint16_t gpio_a_pin,
+          GPIO_TypeDef* gpio_b_port, uint16_t gpio_b_pin,
+          GPIO_TypeDef* gpio_c_port, uint16_t gpio_c_pin);
+  void SetCh(uint8_t ch);
+  uint8_t GetCh() const { return ch_; }
+
+ private:
+  GPIO_TypeDef* gpio_a_port_;
+  uint16_t gpio_a_pin_;
+  GPIO_TypeDef* gpio_b_port_;
+  uint16_t gpio_b_pin_;
+  GPIO_TypeDef* gpio_c_port_;
+  uint16_t gpio_c_pin_;
+};
+}  // namespace ember
+#endif  // EMBER_MODULE_CD4051B_H
---
+++ firmware/ember/src/module/cd4051b.cc
@@ -0,0 +1,27 @@
+#include "ember/module/cd4051b.h"
+
+namespace ember {
+CD4051B::CD4051B(GPIO_TypeDef* gpio_a_port, uint16_t gpio_a_pin,
+                 GPIO_TypeDef* gpio_b_port, uint16_t gpio_b_pin,
+                 GPIO_TypeDef* gpio_c_port, uint16_t gpio_c_pin)
+    : gpio_a_port_(gpio_a_port),
+      gpio_a_pin_(gpio_a_pin),
+      gpio_b_port_(gpio_b_port),
+      gpio_b_pin_(gpio_b_pin),
+      gpio_c_port_(gpio_c_port),
+      gpio_c_pin_(gpio_c_pin) {
+  HAL_GPIO_WritePin(gpio_a_port, gpio_a_pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(gpio_b_port, gpio_b_pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(gpio_c_port, gpio_c_pin, GPIO_PIN_RESET);
+}
+
+void CD4051B::SetCh(uint8_t ch) {
+  if (ch > 8) return;
+  HAL_GPIO_WritePin(gpio_a_port_, gpio_a_pin_,
+                    (ch & 0b00000001) ? GPIO_PIN_SET : GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(gpio_b_port_, gpio_b_pin_,
+                    (ch & 0b00000010) ? GPIO_PIN_SET : GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(gpio_c_port_, gpio_c_pin_,
+                    (ch & 0b00000100) ? GPIO_PIN_SET : GPIO_PIN_RESET);
+}
+}  // namespace ember
