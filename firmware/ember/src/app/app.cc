a6b0328ee6d7b624ab3c06f3d0fbedd4582cb738
Ken Yoshizoe <kenyoshizoe@gmail.com>
2023-08-24 20:24:19 +0900

    :sparkles: add tusb cdc & hid process

--- firmware/ember/src/app/app.cc
+++ firmware/ember/src/app/app.cc
@@ -9,6 +9,8 @@ ember::CD4051B* amux;
 uint16_t adc_val[4];
 bool adc_half_complete = false;
 
+void send_hid_report();
+
 void setup() {
   SEGGER_RTT_Init();
   SEGGER_RTT_printf(0, "Ember startup.\n");
@@ -26,11 +28,24 @@ void setup() {
                                1);
 
   // TinyUSB init
-  tud_init(BOARD_TUD_RHPORT);
+  tusb_init();
 }
 
-void loop() { 
+void loop() {
+  // SEGGER_RTT_printf(0, "loop\n");
   tud_task();
+
+  // USB CDC Echoback Test
+  if (tud_cdc_available()) {
+    send_hid_report();
+
+    char buf[64];
+    uint32_t count = tud_cdc_read(reinterpret_cast<uint8_t*>(buf), 64);
+    (void)count;
+    tud_cdc_write(reinterpret_cast<uint8_t*>(buf), count);
+    tud_cdc_write_flush();
+    SEGGER_RTT_printf(0, "CDC: %s\n", buf);
+  }
 }
 
 void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc) {
@@ -50,8 +65,47 @@ void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc) {
   }
 }
 
+// USB
+// Invoked when device is mounted
+void tud_mount_cb(void) { SEGGER_RTT_printf(0, "USB Connected\n"); }
+// Invoked when device is unmounted
+void tud_umount_cb(void) { SEGGER_RTT_printf(0, "USB Disconnected\n"); }
+// Invoked when usb bus is suspended
+// remote_wakeup_en : if host allow us  to perform remote wakeup
+// Within 7ms, device must draw an average of current less than 2.5 mA from bus
+void tud_suspend_cb(bool remote_wakeup_en) { (void)remote_wakeup_en; }
+// Invoked when usb bus is resumed
+void tud_resume_cb(void) {}
+
+// HID
+union HIDReport {
+  struct {
+    uint8_t modifiers;
+    uint8_t reserved;
+    uint8_t keys[6];
+  } __attribute__((packed)) report;
+  uint8_t raw[8];
+};
+
+void send_hid_report() {
+  // skip if hid is not ready yet
+  if (!tud_hid_ready()) {
+    SEGGER_RTT_printf(0, "HID not ready\n");
+    return;
+  }
+
+  HIDReport report = {0};
+  report.report.keys[0] = 0x04;  // a
+
+  tud_hid_report(0, reinterpret_cast<uint8_t*>(report.raw), sizeof(report));
+}
+
 void tud_hid_report_complete_cb(uint8_t instance, uint8_t const* report,
-                                uint16_t len) {}
+                                uint16_t len) {
+  (void)instance;
+  (void)report;
+  send_hid_report();
+}
 
 // Invoked when received GET_REPORT control request
 // Application must fill buffer report's content and return its length.
@@ -59,6 +113,11 @@ void tud_hid_report_complete_cb(uint8_t instance, uint8_t const* report,
 uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id,
                                hid_report_type_t report_type, uint8_t* buffer,
                                uint16_t reqlen) {
+  (void)instance;
+  (void)report_id;
+  (void)report_type;
+  (void)buffer;
+  (void)reqlen;
   return 0;
 }
 
@@ -66,4 +125,24 @@ uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id,
 // received data on OUT endpoint ( Report ID = 0, Type = 0 )
 void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id,
                            hid_report_type_t report_type, uint8_t const* buffer,
-                           uint16_t bufsize) {}
+                           uint16_t bufsize) {
+  (void)instance;
+  send_hid_report();
+}
+
+// CDC
+// Invoked when cdc when line state changed e.g connected/disconnected
+void tud_cdc_line_state_cb(uint8_t itf, bool dtr, bool rts) {
+  (void)itf;
+  (void)rts;
+
+  // TODO set some indicator
+  if (dtr) {
+    // Terminal connected
+  } else {
+    // Terminal disconnected
+  }
+}
+
+// Invoked when CDC interface received data from host
+void tud_cdc_rx_cb(uint8_t itf) { (void)itf; }
