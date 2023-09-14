757c1082a2fc11b5cdd93baf77293762f0ddef4d
Ken Yoshizoe <kenyoshizoe@gmail.com>
2023-09-14 18:59:57 +0900

    :sparkles: add CD4051B::Init()

--- firmware/ember/include/ember/module/cd4051b.h
+++ firmware/ember/include/ember/module/cd4051b.h
@@ -12,6 +12,7 @@ class CD4051B {
   CD4051B(GPIO_TypeDef* gpio_a_port, uint16_t gpio_a_pin,
           GPIO_TypeDef* gpio_b_port, uint16_t gpio_b_pin,
           GPIO_TypeDef* gpio_c_port, uint16_t gpio_c_pin);
+  void Init();
   void SetCh(uint8_t ch);
   uint8_t GetCh() const { return ch_; }
   void NextCh();
@@ -23,7 +24,7 @@ class CD4051B {
   uint16_t gpio_b_pin_;
   GPIO_TypeDef* gpio_c_port_;
   uint16_t gpio_c_pin_;
-  uint8_t ch_;
+  uint8_t ch_ = 0;
 };
 }  // namespace ember
 #endif  // EMBER_MODULE_CD4051B_H
