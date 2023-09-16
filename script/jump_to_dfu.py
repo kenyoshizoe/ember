7bdd795bb12d6b7ed8da169b568c3ae72751d0f4
Ken Yoshizoe <kenyoshizoe@gmail.com>
2023-09-16 18:18:11 +0900

    :sparkles:  add script to enter dfu

--- script/jump_to_dfu.py
+++ script/jump_to_dfu.py
@@ -1,3 +1,19 @@
 from ember_serial import *
 import serial
-6
+
+# open serial port
+device_name = 'COM3'
+ser = serial.Serial(device_name, timeout=1)
+
+address = 0x3004
+data = [0x00]
+
+write_query = []
+write_query.append(0x01)
+write_query.append(address >> 8)
+write_query.append(address & 0xFF)
+write_query.append(len(data))
+write_query += data
+write_query = cobs.encode(bytes(write_query)) + b'\x00'
+ser.write(write_query)
+ser.flush()
