d2da85133eac47e46b177f28197b9c9b750e633d
kuresugutaira <kuresugutaira@gmail.com>
2023-09-27 18:53:34 +0900

    🔥 removed codes no longer needed.

--- ember-web-configurator/src/app/layout.tsx
+++ ember-web-configurator/src/app/layout.tsx
@@ -1,8 +1,5 @@
 import './globals.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Ember Web-Configurator',
@@ -12,7 +9,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='ja'>
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
