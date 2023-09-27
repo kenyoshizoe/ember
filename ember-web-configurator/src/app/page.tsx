2a44913710a891de227c6ae4a4fcfd6584182efb
kuresugutaira <kuresugutaira@gmail.com>
2023-09-27 18:55:56 +0900

    💄 add a temporary UI for Configurator

--- ember-web-configurator/src/app/page.tsx
+++ ember-web-configurator/src/app/page.tsx
@@ -1,3 +1,21 @@
 export default function Home() {
-  return <></>;
+  return (
+    <main>
+      <h1 className='text-3xl font-bold text-center'>Welcome to Ember Web Configuration</h1>
+      <div className='flex-initial h-32 w-full'>
+        <p className='text-2xl text-center'>connect the keyboard </p>
+      </div>
+      <div className='flex flex-col'>
+        <div className='flex-initial h-32 w-full'>
+          <h2 className='text-2xl text-center'>Key Mapping</h2>
+        </div>
+        <div className='flex-initial h-32 w-full'>
+          <h2 className='text-2xl text-center'>Calibration</h2>
+        </div>
+        <div className='flex-initial h-32 w-full'>
+          <h2 className='text-2xl text-center'>Rapid Trigger</h2>
+        </div>
+      </div>
+    </main>
+  );
 }
